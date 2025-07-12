749e0b3871b72273c370dd17ca3e5287be004602
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-11 19:35:21 -0500

    Create ReportsScreen.tsx

---
+++ frontend-app/src/features/reports/screens/ReportsScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function ReportsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Listado de Reportes</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+});
